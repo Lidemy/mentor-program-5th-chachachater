64fa1b487b9d8bddf768946f0e542a512558d0d6
Sydars <xsw5888xsw@gmail.com>
2021-04-20 23:26:01 +0800

    commit week2 hw1-6

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,7 @@
-function reverse(str) {
-  
+function reverse (str) {
+  let reverse = '';
+  for(let i = 0; i < str.length; i++){
+    reverse += str[str.length - 1 - i]
+  }
+  console.log(reverse);
 }
-
-reverse('hello');
