f395c39df78580155105ee6575d83a462fefdc0f
Sydars <71329979+chachachater@users.noreply.github.com>
2021-04-22 00:22:56 +0800

    Update hw4.js

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,5 @@
 function printFactor (n) {
-  for(let i = 0; i < n; i++){
-    if( n % (i + 1) === 0 ){console.log(i + 1)}
+  for(let i = 1; i < n + 1; i++){
+    if( n % (i) === 0 ){console.log(i)}
   }
 }
