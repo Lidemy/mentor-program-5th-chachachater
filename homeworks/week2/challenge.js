5ebc4e91b8f90005e7526ea362e4482571a20042
Sydars <71329979+chachachater@users.noreply.github.com>
2021-04-22 12:00:25 +0800

    Update challenge.js
    全部打掉重寫了XD，發現其實可以直接做二分搜尋就好

--- homeworks/week2/challenge.js
+++ homeworks/week2/challenge.js
@@ -1,54 +1,29 @@
+//直接做搜尋
 function search (arr, n) {
-    if (arr.length % 2 === 0) {
-        evenArr (arr, n)
-    }
-    else {
-        oddArr (arr, n)
-    }
+    binarySearch (arr, n)
 }
 
-function evenArr (arr, n) { //evenArr指的是 arr 有偶數個 element
+function binarySearch (arr, n) { 
     let start = 0
     let end = arr.length - 1
-    let mid = (start + end) / 2
+    let mid = (end - start) / 2
     let midRight = Math.ceil(mid)
     let midLeft = Math.floor(mid)
+
     if (n >= arr[midRight]) {
         for (let i = midRight; i < arr.length; i++) {
             if (arr[i] === n) {
                 return i
             }
-            return -1
         }
+        return -1
     }
     else {
         for (let i = 0; i <= midLeft; i++) {
             if (arr[i] === n) {
                 return i
             }
-            return -1
         }
-    }
-}
-
-function oddArr (arr, n) { //evenArr指的是 arr 有奇數個 element
-    let start = 0
-    let end = arr.length - 1
-    let mid = (start + end) / 2
-    if (n >= arr[mid]) {
-        for (let i = mid; i < arr.length; i++) {
-            if (arr[i] === n) {
-                return i
-            }
-            return -1
-        }
-    }
-    else {
-        for (let i = 0; i < mid; i++) {
-            if (arr[i] === n) {
-                return i
-            }
-            return -1
-        }
+        return -1
     }
 }
