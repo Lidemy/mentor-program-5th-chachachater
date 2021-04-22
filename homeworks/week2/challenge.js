6d35becae743e3260fabdcce1f5530a4da20af70
Sydars <71329979+chachachater@users.noreply.github.com>
2021-04-22 11:09:07 +0800

    Update challenge.js
    先判斷 arr 是奇數個還是偶數個 element，再分別用不同函式來處理

--- homeworks/week2/challenge.js
+++ homeworks/week2/challenge.js
@@ -1,37 +1,54 @@
-//從 arr 中間砍半開始找
-function search(arr, n) {
-    let len = arr.length
-    if (len % 2 !== 0) {
-        if (n > len / 2) {
-            for (let i = 0; i < Math.floor(len / 2); i++) {
-                if (arr[Math.ceil(len / 2)] + i === n) {
-                    return Math.ceil(len / 2) + i
-                }
+function search (arr, n) {
+    if (arr.length % 2 === 0) {
+        evenArr (arr, n)
+    }
+    else {
+        oddArr (arr, n)
+    }
+}
+
+function evenArr (arr, n) { //evenArr指的是 arr 有偶數個 element
+    let start = 0
+    let end = arr.length - 1
+    let mid = (start + end) / 2
+    let midRight = Math.ceil(mid)
+    let midLeft = Math.floor(mid)
+    if (n >= arr[midRight]) {
+        for (let i = midRight; i < arr.length; i++) {
+            if (arr[i] === n) {
+                return i
             }
-        }
-        else {
-            for (let i = 0; i < Math.ceil(len / 2); i++) {
-                if (arr[i] === n) {
-                    return i
-                }
-            }
+            return -1
         }
     }
     else {
-        if (n > len / 2) {
-            for (let i = 0; i < len / 2; i++) {
-                if (arr[len / 2] === n) {
-                    return i
-                }
+        for (let i = 0; i <= midLeft; i++) {
+            if (arr[i] === n) {
+                return i
             }
-        }
-        else {
-            for (let i = 0; i < len / 2; i++) {
-                if (arr[i] === n) {
-                    return i
-                }
-            }
+            return -1
         }
     }
-    return -1
+}
+
+function oddArr (arr, n) { //evenArr指的是 arr 有奇數個 element
+    let start = 0
+    let end = arr.length - 1
+    let mid = (start + end) / 2
+    if (n >= arr[mid]) {
+        for (let i = mid; i < arr.length; i++) {
+            if (arr[i] === n) {
+                return i
+            }
+            return -1
+        }
+    }
+    else {
+        for (let i = 0; i < mid; i++) {
+            if (arr[i] === n) {
+                return i
+            }
+            return -1
+        }
+    }
 }
