3abeda0dde9695ef046e807553f26ee9374ae841
Sydars <xsw5888xsw@gmail.com>
2021-04-27 09:13:47 +0800

    commit week3 hw1-6

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,50 +1,47 @@
-var readline = require('readline');
-var rl = readline.createInterface({
-    input: process.stdin
-});
-//將input存到lines
-var lines = []
-rl.on('line', function(line) {
-    lines.push(line)
-});
-//回傳結果
-rl.on('close', function() {
-    solution(lines)
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin
+})
+const lines = []
+rl.on('line', (line) => {
+  lines.push(line)
+})
+rl.on('close', () => {
+  solution(lines)
 })
 
-
 function solution(lines) {
-    let group = Number(lines[0])
-    let arr = []
-    for (let i = 1; i < group + 1; i++) {
-        arr.push(lines[i].split(" "))
-    }
+  const group = Number(lines[0])
+  const arr = []
+  for (let i = 1; i < group + 1; i++) {
+    arr.push(lines[i].split(' '))
+  }
 
-
-    for (let i = 0; i < arr.length; i++) {
-        console.log(playGame(arr[i][0], arr[i][1], arr[i][2]))
-    }
+  for (let i = 0; i < arr.length; i++) {
+    console.log(playGame(arr[i][0], arr[i][1], arr[i][2]))
+  }
 }
 
 function playGame(a, b, k) {
+  let tmp
+  if (k === '-1') {
+    tmp = a
+    a = b
+    b = tmp
+  }
 
-    if (k === '-1') {
-        let tmp = a
-        a = b
-        b = tmp
+  const aLength = a.length
+  const bLength = b.length
+
+  if (aLength > bLength) return 'A'
+  else if (aLength < bLength) return 'B'
+  else {
+    for (let i = 0; i < aLength; i++) {
+      if (a[i] > b[i]) return 'A'
+      else if (a[i] < b[i]) return 'B'
+      else { continue }
     }
-
-    let aLength = a.length
-    let bLength = b.length
-
-    if (aLength > bLength) return "A"
-    else if (aLength < bLength) return "B"
-    else {
-        for (let i = 0; i < aLength; i++) {
-            if (a[i] > b[i]) return "A"
-            else if (a[i] < b[i]) return "B"
-            else {continue}
-        }
-            return "DRAW"
-    }
+    return 'DRAW'
+  }
 }
