2a8293c549569a4173cd15c0bf962b5c1fc13dee
Sydars <71329979+chachachater@users.noreply.github.com>
2021-05-07 15:54:44 +0800

    Add files via upload
    上傳自己作的迷你 jest

---
+++ homeworks/week3/jest(test)/challenge.js
@@ -0,0 +1,29 @@
+
+function search (arr, n) {
+    return binarySearch (arr, n)
+}
+
+function binarySearch(arr, n) {
+    let left = 0
+    let right = arr.length - 1
+    let mid = Math.floor((right + left) / 2)
+
+    while (right - left > 1) {
+        if (arr[mid] === n) return mid
+        if (arr[mid] < n) {
+            left = mid + 1
+        }
+        else {
+            right = mid - 1
+        }
+        mid = Math.floor((right + left) / 2)
+    }
+    if (arr[right] === n) return right
+    else if (arr[left] === n) return left
+    else return -1
+
+}
+
+
+exports.search = search
+exports.binarySearch = binarySearch
